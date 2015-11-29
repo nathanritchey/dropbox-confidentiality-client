abb9020b08dce06aa34fd369a3c35b97765e7f6a
Stephen Balhoff <sbalhoff@gmail.com>
2015-11-29 16:29:50 -0600

    encryption.py changes because the last commit did not pick them up.

--- dcc/encryption.py
+++ dcc/encryption.py
@@ -1,17 +1,18 @@
-﻿from base64 import b64encode, b64decode
+from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from pbkdf2 import PBKDF2
 from os import urandom
 
-def encrypt(content, encryption_key, signing_key):
-    iv = urandom(16)
+def encrypt(content, encryption_key, signing_key, iv = None):
+    if iv is None:
+        iv = urandom(16)
     cipher = AES.new(encryption_key, AES.MODE_CBC, iv)
     encrypted = '%s%s' % ( iv, cipher.encrypt(pad(content)) )
     hash = HMAC.new(signing_key, digestmod=SHA256)
     hash.update(encrypted)
     return '%s%s' % (hash.hexdigest(), encrypted)
-
+ 
 def decrypt(content, encryption_key, signing_key):
     signature, contents = ( content[:64], content[64:] )
     hash = HMAC.new(signing_key, digestmod=SHA256)
@@ -23,7 +24,11 @@ def decrypt(content, encryption_key, signing_key):
     return unpad(cipher.decrypt(contents))
 
 BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+
+def padding_length(plaintext_length):
+    return BS - plaintext_length % BS
+
+pad = lambda s: s + padding_length(len(s)) * chr(padding_length(len(s))) 
 unpad = lambda s : s[0:-ord(s[-1])]
 
 def make_key(password): # makes a 256-bit key, a 128-bit iv and a 64-bit password salt
@@ -40,3 +45,4 @@ def get_key(password, key_matter):
     key = key_matter[24:]
     cipher = AES.new(PBKDF2(password, salt).read(32), AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(key))
+
